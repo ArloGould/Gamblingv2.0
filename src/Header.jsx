cc5abcf354a58e220647c03b58918b74f4f405f2
Chaosdemon16 <Arlogould2010@gmail.com>
2025-04-27 16:17:13 +1200

    Added Header Object

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import './styles/Header.css'
+
+ function Header() {
+  return(
+  <div className="Header">
+    <div className="LeftHeader"></div>
+    <div className="CenterHeader"></div>
+    <div className="RightHeader"></div>
+  </div>);
+ }
+export default Header
