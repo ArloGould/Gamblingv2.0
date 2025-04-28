aa38322bb00557f2aca99a5a9aa84a2d02fd9693
Chaosdemon16 <Arlogould2010@gmail.com>
2025-04-28 13:14:33 +1200

    Added _redirects

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,21 +1,21 @@
-import './styles/Header.css'
-import HambugerMenu from './assets/Icons/hamburger-menu.svg'
-import Logo from './assets/TGPLogo.png'
- function Header() {
-  return(
-  <div className="Header">
-    <div className="TopHeader">
-    <img className='Logo' src={Logo}></img>
-    
-    <a href='/Gamblingv2.0/games.html'><button className='MenuItem'>Dashboard</button></a>
-    <a href='/Gamblingv2.0/games.html'><button className='MenuItem'>Games</button></a>
-    <a href='/Gamblingv2.0/games.html'><button className='MenuItem'>Statistics</button></a>
-    
+import { Link } from 'react-router-dom';
+import Logo from './assets/TGPLogo.png';
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="TopHeader">
+        <img className="Logo" src={Logo} alt="Logo" />
+        <Link to="/dashboard" className="MenuItem">Dashboard</Link>
+        <Link to="/games" className="MenuItem">Games</Link>
+        <Link to="/statistics" className="MenuItem">Statistics</Link>
+      </div>
+      <div className="BottomHeader">
+        <p className="C">&#169; 2025</p>
+        <p className="C">v2.0</p>
+      </div>
     </div>
-    <div className="BottomHeader">
-      <p className='C'>&#169; 2025</p>
-      <p className='C'>v2.0</p>
-    </div>
-  </div>);
- }
-export default Header
+  );
+}
+
+export default Header;
