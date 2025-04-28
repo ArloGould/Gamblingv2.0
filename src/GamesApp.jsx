f2de523d8734a54ce57778aaba73f31fec2f4ef6
Chaosdemon16 <Arlogould2010@gmail.com>
2025-04-28 12:26:31 +1200

    Added Header Element

---
+++ src/GamesApp.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './Header.jsx'
+import "./styles/index.css"
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      
+    </>
+  )
+}
+
+export default App
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,21 @@
 import './styles/Header.css'
-
+import HambugerMenu from './assets/Icons/hamburger-menu.svg'
+import Logo from './assets/TGPLogo.png'
  function Header() {
   return(
   <div className="Header">
-    <div className="LeftHeader"></div>
-    <div className="CenterHeader"></div>
-    <div className="RightHeader"></div>
+    <div className="TopHeader">
+    <img className='Logo' src={Logo}></img>
+    
+    <a href='/Gamblingv2.0/games.html'><button className='MenuItem'>Dashboard</button></a>
+    <a href='/Gamblingv2.0/games.html'><button className='MenuItem'>Games</button></a>
+    <a href='/Gamblingv2.0/games.html'><button className='MenuItem'>Statistics</button></a>
+    
+    </div>
+    <div className="BottomHeader">
+      <p className='C'>&#169; 2025</p>
+      <p className='C'>v2.0</p>
+    </div>
   </div>);
  }
 export default Header
